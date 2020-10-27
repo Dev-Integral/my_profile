96a513d8f51588020224998949e54735e4440c54
Ayoola <tayoola13@yahoo.com>
2020-10-27 01:19:52 +0100

    Updates

---
+++ precache-manifest.73390dbf0c164597547dd0934990cb95.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17cf03932d7a922bb99f6d42a4918aef",
+    "url": "/my_profile/index.html"
+  },
+  {
+    "revision": "a8ac47ec0a331061790a",
+    "url": "/my_profile/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "384666898af2cdb14d12",
+    "url": "/my_profile/static/js/2.f742dc2a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my_profile/static/js/2.f742dc2a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a8ac47ec0a331061790a",
+    "url": "/my_profile/static/js/main.3c44772f.chunk.js"
+  },
+  {
+    "revision": "07209ba96f21f911cf62",
+    "url": "/my_profile/static/js/runtime-main.7a6ddded.js"
+  },
+  {
+    "revision": "77475c1619a5fec0889e27c6b17ed205",
+    "url": "/my_profile/static/media/9ja-big.77475c16.png"
+  },
+  {
+    "revision": "f5d86d65a3fef388069e6fcccbec9b59",
+    "url": "/my_profile/static/media/9ja-small.f5d86d65.png"
+  },
+  {
+    "revision": "8cd843c3a530c88dc0b9351538fc44f6",
+    "url": "/my_profile/static/media/about_image.8cd843c3.jpg"
+  },
+  {
+    "revision": "98d2c732df8f33488f079d8ba9638102",
+    "url": "/my_profile/static/media/bootstrap logo.98d2c732.png"
+  },
+  {
+    "revision": "248858b4038103b4e555be6d7ba3c495",
+    "url": "/my_profile/static/media/calc-big.248858b4.png"
+  },
+  {
+    "revision": "4cbd7053560238235686cc467d244fcf",
+    "url": "/my_profile/static/media/clock-big.4cbd7053.png"
+  },
+  {
+    "revision": "4a2f7a770174836992c536fbf311c02b",
+    "url": "/my_profile/static/media/clock-small.4a2f7a77.png"
+  },
+  {
+    "revision": "a9275c1411af1670776ba9ab51123d1a",
+    "url": "/my_profile/static/media/css3 logo.a9275c14.jpg"
+  },
+  {
+    "revision": "8b67d1064dc6b182de524fd187b0331f",
+    "url": "/my_profile/static/media/express.8b67d106.png"
+  },
+  {
+    "revision": "7b9545f6b2e713522b40f29f2734c699",
+    "url": "/my_profile/static/media/integral.7b9545f6.jpg"
+  },
+  {
+    "revision": "c4e307dd27d2f66db493d0ad4764b2ac",
+    "url": "/my_profile/static/media/jquery.c4e307dd.png"
+  },
+  {
+    "revision": "751b067a1e6ca0661b5b4e115b33d6c4",
+    "url": "/my_profile/static/media/js logo.751b067a.png"
+  },
+  {
+    "revision": "c08f88cd880fe30b983433808f5308c7",
+    "url": "/my_profile/static/media/loopback.c08f88cd.png"
+  },
+  {
+    "revision": "a39284d36eead8ebbeb35e3fd9645e12",
+    "url": "/my_profile/static/media/miniflix-big.a39284d3.png"
+  },
+  {
+    "revision": "86e6312566318535ca8d86177c0e21cc",
+    "url": "/my_profile/static/media/miniflix-small.86e63125.png"
+  },
+  {
+    "revision": "6d533353033345ba2f7b696984038eaf",
+    "url": "/my_profile/static/media/mongodb.6d533353.jpg"
+  },
+  {
+    "revision": "9c83821480f50c983788c7c002c72944",
+    "url": "/my_profile/static/media/node logo.9c838214.jpg"
+  },
+  {
+    "revision": "2c4d296a8cce355d6bda9fab9c15cbf1",
+    "url": "/my_profile/static/media/portfolio-big.2c4d296a.png"
+  },
+  {
+    "revision": "843ea208665eeb60b58e40c0f4011eb2",
+    "url": "/my_profile/static/media/portfolio-small.843ea208.png"
+  },
+  {
+    "revision": "aaaadd30f769400400c9fdb5c9262612",
+    "url": "/my_profile/static/media/react logo.aaaadd30.png"
+  },
+  {
+    "revision": "528bfe4df036d317c6a9e219c8b59f9c",
+    "url": "/my_profile/static/media/redux.528bfe4d.png"
+  },
+  {
+    "revision": "f8b385f975ef9f9f92a07769a773889c",
+    "url": "/my_profile/static/media/sc-big.f8b385f9.png"
+  },
+  {
+    "revision": "1240d6479a753be04534aadd352b7919",
+    "url": "/my_profile/static/media/sc-small.1240d647.png"
+  }
+]);
