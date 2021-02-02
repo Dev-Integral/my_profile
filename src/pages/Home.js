4821dba4e6d7b0668c2ff1184b16a297a2ec585a
Ayoola <tayoola13@yahoo.com>
2021-02-02 18:54:45 +0100

    Page completed, Working on functinality

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -11,8 +11,13 @@ import product1 from '../images/product1.jpg';
 import product2 from '../images/product2.jpg';
 import product3 from '../images/product3.jpg';
 import product4 from '../images/product4.jpg';
+/* global $ */
 class PageWrapper extends Component {
     render() {
+        const handleScroll = (incoming) =>{
+            console.log({incoming});
+            $('one').scroll('#home'); 
+        }
         return (
             <div>
                 <header>
@@ -20,24 +25,24 @@ class PageWrapper extends Component {
                         <a href="!#" className="logo" >Portfolio</a>
                         <div className="toggle"></div>
                         <ul>
-                            <li><a href="#1"><i className="fa fa-home"></i><em>Home</em></a></li>
-                            <li><a href="#2"><i className="fa fa-image"></i><em>About</em></a></li>
-                            <li><a href="#3"><i className="fa fa-tasks"></i><em>Services</em></a></li>
-                            <li><a href="#4"><i className="fa fa-id-card"></i><em>Work</em></a></li>
-                            <li><a href="#5"><i className="fa fa-envelope"></i><em>Testimonial</em></a></li>
-                            <li><a href="#6"><i className="fa fa-contact"></i><em>contact</em></a></li>
+                            <li><a href="#home" className='one' onClick={handleScroll('home')}><i className="fa fa-home"></i><em>Home</em></a></li>
+                            <li><a href="#about"><i className="fa fa-image"></i><em>About</em></a></li>
+                            <li><a href="#services"><i className="fa fa-tasks"></i><em>Services</em></a></li>
+                            <li><a href="work"><i className="fa fa-id-card"></i><em>Work</em></a></li>
+                            <li><a href="#testimonial"><i className="fa fa-envelope"></i><em>Testimonial</em></a></li>
+                            <li><a href="#contact"><i className="fa fa-contact"></i><em>contact</em></a></li>
                         </ul>
                     </nav>
                 </header>
-                <section className="banner">
+                <section className="banner" id="home">
                     <div className="textBx">
                         <h2>Hello, I'm</h2>
                         <h2 className="fullname">Ayoola Taiwo.</h2>
                         <h3>I'm a front End Web Developer.</h3>
-                        <a href="#7" class="btn">About Me</a>
+                        <a href="#7" className="btn">About Me</a>
                     </div>
                 </section>
-                <section className="about">
+                <section className="about" id="about">
                     <div className="heading">
                         <h2>About Me</h2>
                     </div>
@@ -60,7 +65,7 @@ class PageWrapper extends Component {
                         </div>
                     </div>
                 </section>
-                <section className="services">
+                <section className="services" id="services">
                     <div className="heading white">
                         <h2>Our Services</h2>
                         <p>Lorem ipsum smelfgm flgdmdf</p>
@@ -104,7 +109,7 @@ class PageWrapper extends Component {
                         </div>
                     </div>
                 </section>
-                <section className="work">
+                <section className="work" id="work">
                     <div className="heading">
                         <h2>Our Latest Work</h2>
                         <p>Lorem ipsum smelfgm flgdmdf fm vgfkjgnlg mbfd.sdfjnfg g fg
@@ -128,7 +133,7 @@ class PageWrapper extends Component {
                             <a className="btn" href="#8">View more</a></div>
                     </div>
                 </section>
-                <section className="testimonials">
+                <section className="testimonials" id="testimonial">
                     <div className="heading">
                         <h2>Testimonials</h2>
                         <p>Lorem ipsum smelfgm flgdmdf fm vgfkjgnlg mbfd.sdfjnfg g fg
@@ -141,17 +146,80 @@ class PageWrapper extends Component {
                                 coomwoc owc c wcmopqn qoqpjei  m,zlkaslqow okmc;aw moow ca;amowieurcnc ccoww. jnon csjcnownc
                                 cocmlswonweweoi jnwcncodpwnl nmooeincnc ikdodd idwoiwoqp  wqjiwopqwiw 
                             </p>
-                            <h3>Someonr Famous<br /><span> Creative Designer</span></h3>
+                            <h3>Someone Famous Designer<br /><span> Creative Designer</span></h3>
+                        </div>
+                        <div className="testimonialBx">
+                            <p>Lorem csc kdclkkonmcd xcniw wina.a;ic ckmlcow c,cnowanoaoscn diocn saoc oqw ewnn ud diqomdqmo kdclkkonmcd
+                                coomwoc owc c wcmopqn qoqpjei  m,zlkaslqow okmc;aw moow ca;amowieurcnc ccoww. jnon csjcnownc
+                                cocmlswonweweoi jnwcncodpwnl nmooeincnc ikdodd idwoiwoqp  wqjiwopqwiw 
+                            </p>
+                            <h3>Someone Famous Designer<br /><span> Creative Designer</span><br /><button className="btn">Hello</button></h3>
                         </div>
                     </div>
                 </section>
+                <section className="contact" id="contact">
+                <div className="heading white">
+                    <h2>Contact Us</h2>
+                    <p>Lorem ipsum smelfgm flgdmdf fm vgfkjgnlg mbfd.sdfjnfg</p>
+                </div>
+                <div className="content">
+                    <div className="contactInfo">
+                        <h3>Contact Info</h3>
+                        <div className="contactInfoBx">
+                            <div className="box">
+                                <div className="icon">
+                                <i className="fa fa-map-marker"></i>
+                                </div>
+                                <div className="text">
+                                    <h3>Address</h3>
+                                    <p>464523 super doper,<br /> hotels nigria,<br />234 4567 4567</p>
+                                </div>
+                            </div>
+                            <div className="box">
+                                <div className="icon">
+                                <i className="fa fa-phone"></i>
+                                </div>
+                                <div className="text">
+                                    <h3>Phone</h3>
+                                    <p>464-523-2354</p>
+                                </div>
+                            </div>
+                            <div className="box">
+                                <div className="icon">
+                                    <i className="fa fa-envelope-o"></i>
+                                </div>
+                                <div className="text">
+                                    <h3>Email</h3>
+                                    <p>temporary@mail.net</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="formBx">
+                        <form>
+                            <h3>Message Me</h3>
+                            <input type="text" name="" placeholder="Full Name" />
+                            <input type="email" name="" placeholder="Email" />
+                            <textarea placeholder="Your Mesage"></textarea>
+                            <button type="submit">Send</button>
+                        </form>
 
+                    </div>
+                </div>
+                </section>
+                <div className="copyright">
+                    <p>Copyright &copy; 2021 Integral Practicals. All Rights Reserved</p>
+                </div>
 
                 <script type="text/javascript">
                 {window.addEventListener('scroll', function(){
                     let nav = document.querySelector('nav');
                     nav.classList.toggle('sticky', window.scrollY >0); 
-                })};
+                })
+                //onClick={e=>{
+                  //    $('.myFile').trigger('click');
+                  //}}
+                };
                 </script>
                 {this.props.children}
             </div>
