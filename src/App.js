c643be81079eec895c10ddbd2b29a597e617d862
Ayoola <tayoola13@yahoo.com>
2021-02-06 09:56:56 +0100

    up and running

--- src/App.js
+++ src/App.js
@@ -1,22 +1,60 @@
 import React from 'react';
-import Home from './pages/Home';
-import { Route, BrowserRouter as Router } from 'react-router-dom';
-function App() {
+import './App.css'
+import logo from './images/logo.png'
+const App= () => {
     return (
-        <Router>
-            <Route
-                path='/'
-                exact={true}
-                render={(props) => {
-                    return (
-                        <div className="App">
-                            <Home />
+        <div className="overall">
+            <header>
+                <div className="container">
+                    <nav>
+                        <div className="nav-brand">
+                            <a href="!#">
+                                <img src={logo} alt="logo" />
+                            </a>
                         </div>
-
-                    )
-                }}
-            />
-        </Router>
+                        <div className="menu-icons open">
+                            <i className="icon ion-md-close"></i>
+                        </div>
+                        <ul className="nav-list">
+                            <div className="menu-icons close">
+                                <i className="icon ion-md-close"></i>
+                            </div>
+                            <li className="nav-item">
+                                <a href className="nav-link">Home</a>
+                            </li>
+                            <li className="nav-item">
+                                <a href className="nav-link">Destinations</a>
+                            </li>
+                            <li className="nav-item">
+                                <a href className="nav-link">Pricing</a>
+                            </li>
+                            <li className="nav-item">
+                                <a href className="nav-link">Booking</a>
+                            </li>
+                            <li className="nav-item">
+                                <a href className="nav-link">About</a>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
+            </header>
+            <section className="hero">
+                <div className="container">
+                    <div className="main-message">
+                        <h3>The grat outdoor</h3 >
+                        <h1>Adventure</h1>
+                        <p>
+                            lorem ipsum dolor sit aret, consectour elit. conssecteur
+                             ex, ipsum minus nolestias odit praseunium tenatoee vitae voluptates Ab autem debitis error hic 
+                             id illum nolestiae ratione. Minus?
+                        </p>
+                        <div className="cta">
+                            <a href="!#" className="btn">Book now</a>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </div>
     )
 }
 
